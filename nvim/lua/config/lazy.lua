aed2b72640b411f0da15173b1eacb8c79d0d7332
Basil Meer <basilmeer@gmail.com>
2025-01-23 14:43:27 +0500

    silence plugin updates on start

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 require("config.lualine")
